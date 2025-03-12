d57e445e438ae24e816e8e09d92fc17bfaa94d87
em-latic <emanuel.munoz-orlich@outlook.com>
2025-03-12 16:11:04 -0600

    Translation logic included

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,26 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
+    translate(phrase, origin) {
+        let dictionary = {};
+        let terms = [];
+
+        if(origin == "american"){
+            dictionary = { ...americanOnly, ...americanToBritishSpelling, ...americanToBritishTitles };
+            terms = Object.keys(dictionary);
+            // terms = Object.keys(americanOnly);
+            // terms.concat(Object.keys(americanToBritishSpelling));
+            // terms.concat(Object.keys(americanToBritishTitles));
+        }
+        else {
+            dictionary = {...britishOnly};
+            terms = Object.keys(britishOnly);
+        }
+        const regexDictionary = new RegExp("\\b(" + terms.join("|") + ")\\b"); //, "gi"
+        const translation = phrase.replace(regexDictionary, match => dictionary[match.toLowerCase()]);
+
+        return translation;
+    }
 }
 
 module.exports = Translator;
